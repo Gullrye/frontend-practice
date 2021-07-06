ea51a127f733aa7989c4ae75b185ad67bff7432f
Gullrye <wmwspecter@163.com>
2021-07-07 00:18:36 +0800

    函数封装

--- 26键盘/main.js
+++ 26键盘/main.js
@@ -1,83 +1,16 @@
 // 1. 初始化数据
-let keyRows = [
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
-]
-// Crtl + F 使用 正则 ([a-z])   ->   '$1',
-// 来添加keys中字母的引号和字母后的逗号
-
-let hash = {
-  q: 'qq.com',
-  w: 'weibo.com',
-  e: 'ele.me',
-  r: 'renren.com',
-  t: '.com',
-  y: '.com',
-  u: 'ubuntu.com',
-  i: '.com',
-  o: 'opera.com',
-  p: '.com',
-  a: '.com',
-  s: '.com',
-  d: '.com',
-  f: '.com',
-  g: '.com',
-  h: '.com',
-  j: '.com',
-  k: '.com',
-  l: '.com',
-  z: 'zhihu.com',
-  x: 'xiecheng.com',
-  c: '.com',
-  v: '.com',
-  b: 'bilibili.com',
-  n: '.com',
-  m: 'meituan.com',
-  length: 26,
-}
-
-// 取出localStorage 中的 z 对应的 hash
-let hashInLocalStorage = getFromLocalStorage('zzz')
-if (hashInLocalStorage) {
-  hash = hashInLocalStorage
-}
+let hashA = init()
+let keyRows = hashA['keys']
+let hash = hashA['hash']
 
 // 2. 生成键盘
-let keyBox = document.getElementsByClassName('key-box')[0]
+generateKeyboard(keyRows, hash)
 
-for (let i = 0; i < keyRows.length; i++) {
-  let ul = tag('ul')
+// 3. 监听用户动作
+ListenToUser(hash)
 
-  keyBox.appendChild(ul)
-  
-  for (let j = 0; j < keyRows[i].length; j++) {
-    let span = tag('span', { textContent: keyRows[i][j] })
-    
-    let editBtn = createButton(keyRows[i][j])
-    
-    let img = createImage(hash[keyRows[i][j]])
-    
-    
-    let li = tag('li', { className: 'key' })
 
-    li.appendChild(span)
-    li.appendChild(editBtn)
-    li.appendChild(img)
-    ul.appendChild(li)
-    
-  }
-}
-
-// 3. 监听键盘
-// 按键后在新窗口打开相应网站
-document.onkeydown = (e) => {
-  console.log(e)
-  let website = hash[e.key]
-
-  window.open('https://' + website)
-}
-
+// 下面是工具函数
 function getFromLocalStorage(name) {
   return JSON.parse(localStorage.getItem(name) || 'null')
 }
@@ -93,19 +26,19 @@ function tag(tagName, attributes) {
 
 function createButton(buttonId) {
   let editBtn = tag('button', { textContent: 'E', id: buttonId })
-    editBtn.onclick = (e) => {
-      let img2 = e['target'].nextSibling // 下一个兄弟元素
-      let editWebsite = prompt('输入一个网址：')
-      
-      hash[e['target']['id']] = editWebsite
-      img2.src = 'http://' + editWebsite + '/favicon.ico'
-      img2.onerror = (e) => {
-        e.target.src = 'images/default_ico32.png'
-      }
-      localStorage.setItem('zzz', JSON.stringify(hash))
-    }
+  editBtn.onclick = (e) => {
+    let img2 = e['target'].nextSibling // 下一个兄弟元素
+    let editWebsite = prompt('输入一个网址：')
 
-    return editBtn
+    hash[e['target']['id']] = editWebsite
+    img2.src = 'http://' + editWebsite + '/favicon.ico'
+    img2.onerror = (e) => {
+      e.target.src = 'images/default_ico32.png'
+    }
+    localStorage.setItem('zzz', JSON.stringify(hash))
+  }
+
+  return editBtn
 }
 
 function createImage(domain) {
@@ -120,4 +53,88 @@ function createImage(domain) {
   }
 
   return img
+}
+
+function init() {
+  let keyRows = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+  ]
+  // Crtl + F 使用 正则 ([a-z])   ->   '$1',
+  // 来添加keys中字母的引号和字母后的逗号
+
+  let hash = {
+    q: 'qq.com',
+    w: 'weibo.com',
+    e: 'ele.me',
+    r: 'renren.com',
+    t: '.com',
+    y: '.com',
+    u: 'ubuntu.com',
+    i: '.com',
+    o: 'opera.com',
+    p: '.com',
+    a: '.com',
+    s: '.com',
+    d: '.com',
+    f: '.com',
+    g: '.com',
+    h: '.com',
+    j: '.com',
+    k: '.com',
+    l: '.com',
+    z: 'zhihu.com',
+    x: 'xiecheng.com',
+    c: '.com',
+    v: '.com',
+    b: 'bilibili.com',
+    n: '.com',
+    m: 'meituan.com',
+    length: 26,
+  }
+
+  // 取出localStorage 中的 z 对应的 hash
+  let hashInLocalStorage = getFromLocalStorage('zzz')
+  if (hashInLocalStorage) {
+    hash = hashInLocalStorage
+  }
+  return {
+    keys: keyRows,
+    hash: hash,
+  }
+}
+
+function generateKeyboard(keyRows, hash) {
+  let keyBox = document.getElementsByClassName('key-box')[0]
+
+  for (let i = 0; i < keyRows.length; i++) {
+    let ul = tag('ul')
+
+    keyBox.appendChild(ul)
+
+    for (let j = 0; j < keyRows[i].length; j++) {
+      let span = tag('span', { textContent: keyRows[i][j] })
+
+      let editBtn = createButton(keyRows[i][j])
+
+      let img = createImage(hash[keyRows[i][j]])
+
+      let li = tag('li', { className: 'key' })
+
+      li.appendChild(span)
+      li.appendChild(editBtn)
+      li.appendChild(img)
+      ul.appendChild(li)
+    }
+  }
+}
+
+function ListenToUser(hash) {
+  document.onkeydown = (e) => {
+    console.log(e)
+    let website = hash[e.key]
+
+    window.open('https://' + website)
+  }
 }
