331642c35ec99dcd69c83f10c190ae1d1c473d9a
Gullrye <wmwspecter@163.com>
2021-07-10 22:10:14 +0800

    add filterbar

--- 简历/main.js
+++ 简历/main.js
@@ -1,9 +1,22 @@
-header = document.querySelector('.header-inner')
 
+let header = document.querySelector('.header-inner')
+
+// 导航栏动画
 window.onscroll = () => {
   if (window.scrollY > 0) {
     header.classList.add('active')
   } else {
     header.classList.remove('active')
+  }
+}
+
+// 作品部分filter栏切换
+
+let btns = document.querySelectorAll('.btn-group button')
+let filterBarSpan = document.querySelector('.filter-bar span')
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = () => {
+    filterBarSpan.className = `state${i+1}`
   }
 }
