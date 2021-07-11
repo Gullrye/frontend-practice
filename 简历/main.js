d3aef377fe70caf4fd7c64b2c39b577db312b3a1
Gullrye <wmwspecter@163.com>
2021-07-11 14:17:49 +0800

    添加导航栏mouseenter动画

--- 简历/main.js
+++ 简历/main.js
@@ -1,7 +1,7 @@
 // 导航栏动画
 let headerTop = document.querySelector('header')
 window.onscroll = () => {
-  if (window.scrollY > 0) { 
+  if (window.scrollY > 0) {
     headerTop.classList.add('sticky')
   } else {
     headerTop.classList.remove('sticky')
@@ -15,12 +15,28 @@ let filterBarSpan = document.querySelector('.filter-bar span')
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].onclick = () => {
-    filterBarSpan.className = `state${i+1}`
+    filterBarSpan.className = `state${i + 1}`
   }
 }
 
 // 加载动画
 let startPage = document.querySelector('.start-page')
-setTimeout(function(){
+setTimeout(function () {
   startPage.classList.remove('active')
-}, 2000)
+}, 1000)
+
+// 鼠标进入导航栏
+let Tages = document.querySelectorAll('.nav-wrap > ul > li')
+
+Tages.forEach(function (i) {
+  let subMenu = i.querySelector('.submenu')
+
+  if (subMenu) {
+    i.onmouseenter = () => {
+      subMenu.classList.add('active')
+    }
+    i.onmouseleave = () => {
+      subMenu.classList.remove('active')
+    }
+  }
+})
