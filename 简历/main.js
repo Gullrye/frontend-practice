c533b28bb262b4626ce5f33488516a4fdb6d9488
Gullrye <wmwspecter@163.com>
2021-07-11 17:18:15 +0800

    滚动至指定元素

--- 简历/main.js
+++ 简历/main.js
@@ -27,33 +27,45 @@ setTimeout(function () {
 
 // 鼠标进入导航栏
 // 下拉菜单
-let Tags = document.querySelectorAll('.nav-wrap > ul > li > a')
-let Li = document.querySelectorAll('.nav-wrap > ul > li')
+let aTags = document.querySelectorAll('.nav-wrap > ul > li > a')
+let LiTags = document.querySelectorAll('.nav-wrap > ul > li')
 
-for (let i = 0; i < Li.length; i++) {
-  let subMenu = Li[i].querySelector('.submenu')
+for (let i = 0; i < LiTags.length; i++) {
+  let subMenu = LiTags[i].querySelector('.submenu')
 
   if (subMenu) {
-    Li[i].onmouseenter = () => {
+    LiTags[i].onmouseenter = () => {
       subMenu.classList.add('active')
     }
-    Li[i].onmouseleave = () => {
+    LiTags[i].onmouseleave = () => {
       subMenu.classList.remove('active')
     }
   }
 }
 
 // 切换条
-for (let i = 0; i < Li.length; i++) {
+for (let i = 0; i < LiTags.length; i++) {
   let spanBar = document.querySelectorAll('.nav-wrap > ul > li > span')
   let index = i
 
   // js控制width后，动画才有恢复过程，使用css animation时鼠标移开后切换条会直接消失，而不是从右往左变短至0
-  Tags[i].onmouseenter = () => {
+  aTags[i].onmouseenter = () => {
     spanBar[index].style.width = '100%'
   }
 
-  Tags[i].onmouseleave = () => {
+  aTags[i].onmouseleave = () => {
     spanBar[index].style.width = '0'
   }
+
+  // 滚动至指定元素
+  aTags[i].onclick = (x) => {
+    x.preventDefault()
+    let href = x.currentTarget.getAttribute('href') // '#about' ...
+    let element = document.querySelector(href)
+    let top = element.offsetTop
+
+    window.scrollTo(0, top + 110)
+  }
 }
+
+
