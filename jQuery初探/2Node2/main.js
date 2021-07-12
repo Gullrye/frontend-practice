57418687a8c2df8daf62644a404f83ed3c4da0c6
Gullrye <wmwspecter@163.com>
2021-07-12 18:05:48 +0800

    add Node2

---
+++ jQuery初探/2Node2/main.js
@@ -0,0 +1,30 @@
+window.Node2 = function (node) {
+  return {
+    getSiblings: function () {
+      let allChildren = node.parentNode.children
+      let array = {
+        length: 0,
+      }
+
+      for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== node) {
+          array[array.length] = allChildren[i]
+          array.length += 1
+        }
+      }
+      return array
+    },
+    addClass: function (classes) {
+      for (let key in classes) {
+        let value = classes[key]
+        let methodName = value ? 'add' : 'remove'
+
+        node.classList[methodName](key)
+      }
+    },
+  }
+}
+
+var node2 = Node2(item2)
+console.log(node2.getSiblings())
+node2.addClass({ a: true, b: false, c: true })
