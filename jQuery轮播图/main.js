63c7fde8f2ffcba03b1a7c8c018037faf4788e6b
Gullrye <wmwspecter@163.com>
2021-07-13 12:44:21 +0800

    add auto slide

--- jQuery轮播图/main.js
+++ jQuery轮播图/main.js
@@ -1,13 +1,35 @@
-let allButtons = $('.btn-group > span')  // 伪数组
+let allButtons = $('.btn-group > span') // 伪数组
 
-for (let i = 0; i< allButtons.length; i++) {
+for (let i = 0; i < allButtons.length; i++) {
   // console.log(allButtons[i]); // 不是jQuery对象，而是所获取的标签
   // console.log($(allButtons[i]));  // 通过$将标签转化为了jQuery对象
 
   let index = i
-  $(allButtons[i]).on('click', function(x) {
-    let distance = index * (-300)
+  $(allButtons[i]).on('click', function (x) {
+    let distance = index * -300
     $('.img-wrap').css('transform', `translateX(${distance}px)`)
     $(x.currentTarget).addClass('active').siblings().removeClass('active')
+
+    n = index
   })
+}
+
+let n = 0
+let timer = setTimer()
+
+$('.img-window').on('mouseenter', function () {
+  clearInterval(timer)
+})
+$('.img-window').on('mouseleave', function () {
+  timer = setTimer()
+})
+
+function setTimer() {
+  return setInterval(function () {
+    if (n > 2) {
+      n = n % 3
+    }
+    $(allButtons).eq(n).trigger('click')
+    n += 1
+  }, 1000)
 }
