8e00706b4cfb1c8a992679e397711fd91139fc01
Gullrye <wmwspecter@163.com>
2021-07-13 23:16:02 +0800

    add visibilitychange

--- 无缝轮播/main.js
+++ 无缝轮播/main.js
@@ -2,7 +2,7 @@ $('.img-wrap img:nth-child(1)').addClass('current')
 $('.img-wrap img:nth-child(2)').addClass('enter')
 $('.img-wrap img:nth-child(3)').addClass('enter')
 let n = 1
-setInterval(() => {
+let timer = setInterval(() => {
   $(`.img-wrap img:nth-child(${x(n)})`)
     .removeClass('current')
     .addClass('leave')
@@ -14,6 +14,27 @@ setInterval(() => {
     .addClass('current')
   n += 1
 }, 3000)
+
+// 无以下visibilitychange相关的代码会有bug，即好几次的轮播切换会在你浏览其他网页回来后一次性播放
+// 离开当前页面时，保持当前图片不变。
+document.addEventListener('visibilitychange', function(e) {
+  if(document.hidden) {
+    window.clearInterval(timer)
+  } else {
+    timer = setInterval(() => {
+      $(`.img-wrap img:nth-child(${x(n)})`)
+        .removeClass('current')
+        .addClass('leave')
+        .one('transitionend', (e) => {
+          $(e.currentTarget).removeClass('leave').addClass('enter')
+        })
+      $(`.img-wrap img:nth-child(${x(n + 1)})`)
+        .removeClass('enter')
+        .addClass('current')
+      n += 1
+    }, 3000)
+  }
+})
 
 function x(n) {
   if (n > 3) {
