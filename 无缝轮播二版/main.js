f3a57177b9b9ffe9497990bb6939dd128dea3378
Gullrye <wmwspecter@163.com>
2021-07-25 22:49:56 +0800

    函数节流

--- 无缝轮播二版/main.js
+++ 无缝轮播二版/main.js
@@ -12,7 +12,12 @@ $slides.css({transform: 'translateX(-400px)'})
 
 let current = 0
 
+let lock = true
+
 $buttons.eq(0).on('click', function () {
+  if (!lock) return
+  lock = false
+
   if (current == 2) {
     console.log('end to start')
     $slides.css({transform: 'translateX(-1600px)'}).one('transitionend', function () {
@@ -24,12 +29,26 @@ $buttons.eq(0).on('click', function () {
     $slides.css({transform: 'translateX(-400px)'})
   }
   current = 0
+
+  setTimeout(function () {
+    lock = true
+  }, 1000)
 })
 $buttons.eq(1).on('click', function () {
+  if (!lock) return
+  lock = false
+
   $slides.css({transform: 'translateX(-800px)'})
   current = 1
+
+  setTimeout(function () {
+    lock = true
+  }, 1000)
 })
 $buttons.eq(2).on('click', function () {
+  if (!lock) return
+  lock = false
+
   if (current == 0) {
     console.log('start to end')
     $slides.css({transform: 'translateX(0px)'}).one('transitionend', function () {
@@ -40,4 +59,8 @@ $buttons.eq(2).on('click', function () {
     $slides.css({transform: 'translateX(-1200px)'})
   }
   current = 2
+
+  setTimeout(function () {
+    lock = true
+  }, 1000)
 })
